1cd73487cae92ee73fb34c5b90a591bfaec2aea0
wael ahmed <w.ahmed020@gmail.com>
2021-03-11 20:00:18 +0000

    js code updates

---
+++ pseudo-code.js
@@ -0,0 +1,52 @@
+
+//[start function]
+// funtion to start the game with start button
+// when you start the game the start button is hidden
+// when the start button is hidden the display screen and answer buttons appear
+// >> (triggers Qs function) the display screen contains the first questions
+
+
+
+
+//[timer Function]
+// the start game button also starts a timer function
+// the timer ticks down from 20
+// if the right answer is selected the page will move to the next QS (seperate function) and timer restarts
+// if the timer gets to zero, the (end game function kicks in) 
+// the display screen  shows a game over message (sepearat function)
+
+
+
+//[Qs function]
+// the qs function is activated once the user presses start
+// the function looks at a list of questions and selects one at random
+// the questions is displayed on the display screen
+// the answers to question are shown on the answer buttons
+//
+
+
+
+
+
+//[End game function]
+// when the timer gets to 0 or an inncorrect answer is pressed the game ends
+// the display screen shows game over on the screen 
+// the answer buttons are removed
+// the submit name section appears below the screen
+
+
+//[submit function] 
+// when the user enters name and clicks the submit button 
+// the display screen shows name of user and score
+// the submit button is submit area is also replaced by a 'Play Again' button.
+
+
+//[Play Again function]
+// when the play agian button is pressed, the game is pressed the start game function is activated
+
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -9,7 +9,6 @@ let buttonEl = document.querySelectorAll("section > button");
 let startbtnEl = document.getElementById("start-btn");
 let restartbtnEl = document.getElementById("restart-btn");
 
-
 //============================================
 // start game button
 const startGameEl = document.querySelector(".start-btn");
@@ -22,130 +21,112 @@ let timerEl = document.querySelector(".timer");
 
 //============================================
 
-startGameEl.addEventListener("click", function (){ //game start button
-
-    var startButton = startbtnEl.classList.add("hide");
-    var screen =  screenEl.classList.remove("hide");
-    var navigation = navEl.classList.remove("hide");
-    var answerButton = answerBtnEl.classList.remove("hide");
-    
-    let mode = "light";
-
-    themeSwitcher.addEventListener("click", function(){
-        if(mode === "light"){
-            mode = "dark";
-            htmlEl.setAttribute("class", "dark"); 
-            headerEl.setAttribute("class", "header-bar-dark"); 
-            screen.setAttribute("class", "screen-black hide");
-            
-            for(i = 0; i < buttonEL.length; i++) {
-                buttonEl[i].setAttribute("class", "ans-btn-dark") 
-            
-                }
-
-        } else {
-            mode = "light";
-            htmlEl.setAttribute("class", "light");
-            headerEl.setAttribute("class", "header-bar") 
-            screen.setAttribute("class", "screen")
-            
-            for(i = 0; i < buttonEl.length; i++) {
-                buttonEl[i].setAttribute("class", "ans-btn")   
-            
-                }
-
-        }
-
-    })
-
-});
-
-
-startGameEl.addEventListener("click", function setTimer(){
-
-    var seconds = 20;
-
-    var timerInterval = setInterval(function() {
-        seconds--;
-        timerEl.textContent = "Time:" + " " + seconds;
-
-        // if(correct answer is clicekd){
-
-            // then restart the timer and move to the next question 
-            // return timerInterval
-            // nextQuestion
-
-        // } else 
-    
-        if(seconds === 0) {
-
-          timerEl.textContent = 'Time: 0';  
-         
-          clearInterval(timerInterval);
-          
-          endGame();
-        }
-    
-      }, 1000);
-         
-})
-
-
-
-
-// ============================================
-
 let mode = "light";
 
 //theme switching before the game starts
 themeSwitcher.addEventListener("click", function() {
-
   // If mode is dark, apply light background
   if (mode === "light") {
     mode = "dark";
-    htmlEl.setAttribute("class", "dark"); 
-    headerEl.setAttribute("class", "header-bar-dark"); 
-    startbtnEl.setAttribute("class","start-btn-dark");
+    htmlEl.setAttribute("class", "dark");
+    headerEl.setAttribute("class", "header-bar-dark");
+    // startbtnEl.setAttribute("class", "start-btn-dark");
   }
-  // If mode is light, apply dark background 
+  // If mode is light, apply dark background
   else {
     mode = "light";
     htmlEl.setAttribute("class", "light");
     headerEl.setAttribute("class", "header-bar");
-    startbtnEl.setAttribute("class","start-btn");
-   
+    // startbtnEl.setAttribute("class", "start-btn");
   }
 });
 
-// startGameEl.addEventListener("click", function setTimer(){
+//=======================================
 
-    
+startGameEl.addEventListener("click", function () {
+  //game start button
+
+  var startButton = startbtnEl.classList.add("hide");
+  var restartbtn = restartbtnEl.classList.add("hide");
+  var screen = screenEl.classList.remove("hide");
+  var navigation = navEl.classList.remove("hide");
+  var answerButton = answerBtnEl.classList.remove("hide");
+
+  let mode = "light";
+
+  themeSwitcher.addEventListener("click", function () {
+    //is there anythin i can add here?
+    if (mode === "light") {
+      mode = "dark";
+      htmlEl.setAttribute("class", "dark");
+      headerEl.setAttribute("class", "header-bar-dark");
+      screen.setAttribute("class", "screen-black hide"); //remove
+      startbtnEl.setAttribute("class", "hide"); // remove
+
+      for (i = 0; i < buttonEL.length; i++) {
+        buttonEl[i].setAttribute("class", "ans-btn-dark");
+      }
+    } else {
+      mode = "light";
+      htmlEl.setAttribute("class", "light");
+      headerEl.setAttribute("class", "header-bar");
+      screen.setAttribute("class", "screen"); //remove
+      startbtnEl.setAttribute("class", "hide"); // remove
+
+      for (i = 0; i < buttonEl.length; i++) {
+        buttonEl[i].setAttribute("class", "ans-btn");
+      }
+    }
+  });
+});
+
+startGameEl.addEventListener("click", function setTimer() {
+  var seconds = 20;
+
+  var timerInterval = setInterval(function () {
+    seconds--;
+    timerEl.textContent = "Time:" + " " + seconds;
+
+    // if(correct answer or next/ previous button is clicekd){
+
+    // then restart the timer and move to the next question
+    // return timerInterval
+    // nextQuestion
+
+    // } else
+
+    if (seconds === 0) {
+      timerEl.textContent = "Time: 0";
+
+      clearInterval(timerInterval);
+
+      endGame();
+    }
+  }, 1000);
+});
+
+// ============================================
+
+// startGameEl.addEventListener("click", function setTimer(){
 
 // }
 
 
 
 
-// -1 funtion to start the game with start button - this also starts the timer
-// you press the start button and it kick starts the timer.
-// at same time the start button is hidden
-// then the hidden functions are unhidden except for the restart and submit form button.
-// the screen shows the first question
-// the answer choices are shown 
+function endGame() {
 
 
-
-//-2 function to set the next question
-//  set an array of questions
+}
 
 
-//- function to select answer
+submitButtonEl.addEventListener("click", function submit(){
 
 
-// funtion to take you to the next question or previous question
+})
 
 
-//-3 results page and submit button
+playAgainEl.addEventListener("click", function playAgain(){
 
-
-
+    
+})
