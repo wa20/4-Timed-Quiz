7fdc3402e19e65d551684f73357f0d7b5f7edb7a
wael ahmed <w.ahmed020@gmail.com>
2021-03-11 11:30:10 +0000

    code update and draft file js creation

--- script.js
+++ script.js
@@ -1,35 +1,4 @@
 //============================================
-// timer element
-let startGameEl = document.querySelector(".start-btn");
-let timerEl = document.querySelector(".timer");
-
-
-
-
-startGameEl.addEventListener("click", function setTimer(){
-
-    var seconds = 20;
-
-    var timerInterval = setInterval(function() {
-        seconds--;
-        timerEl.textContent = "Time:" + " " + seconds;
-    
-        if(seconds < 0) {
-
-          timerEl.textContent = 'Time: 00';  
-         
-          clearInterval(timerInterval);
-          
-          endGame();
-        }
-    
-      }, 1000);
-
-})
-
-
-
-//============================================
 // light and Dark Theme Function.
 
 // Access toggle switch HTML element
@@ -49,11 +18,11 @@ themeSwitcher.addEventListener("click", function() {
   // If mode is dark, apply light background
   if (mode === "light") {
     mode = "dark";
-    htmlEl.setAttribute("class", "dark");
-    headerEl.setAttribute("class", "header-bar-dark")
-    screenEl.setAttribute("class", "screen-black hide")
-    startbtnEl.setAttribute("class","start-btn-dark")
-    restartbtnEl.setAttribute("class", "restart-btn-dark hide")
+    htmlEl.setAttribute("class", "dark"); 
+    headerEl.setAttribute("class", "header-bar-dark") 
+    screenEl.setAttribute("class", "screen-black hide") // needs to be fixed
+    startbtnEl.setAttribute("class","start-btn-dark") // needs to be fixed
+    restartbtnEl.setAttribute("class", "restart-btn-dark hide") // needs to be fixed
     
     for(i = 0; i < buttonEL.length; i++) {
     buttonEL[i].setAttribute("class", "ans-btn-dark") 
@@ -64,10 +33,10 @@ themeSwitcher.addEventListener("click", function() {
   else {
     mode = "light"
     htmlEl.setAttribute("class", "light");
-    headerEl.setAttribute("class", "header-bar")
-    screenEl.setAttribute("class", "screen hide")
-    startbtnEl.setAttribute("class","start-btn")
-    restartbtnEl.setAttribute("class", "restart-btn hide")
+    headerEl.setAttribute("class", "header-bar") 
+    screenEl.setAttribute("class", "screen hide") // needs to be fixed
+    startbtnEl.setAttribute("class","start-btn") // needs to be fixed
+    restartbtnEl.setAttribute("class", "restart-btn hide")  // needs to be fixed
     
     for(i = 0; i < buttonEL.length; i++) {
     buttonEL[i].setAttribute("class", "ans-btn")   
@@ -76,10 +45,71 @@ themeSwitcher.addEventListener("click", function() {
   }
 });
 
+//============================================
+
+
+
+// timer element
+let startGameEl = document.querySelector(".start-btn");
+let timerEl = document.querySelector(".timer");
+
+
+
+
+startGameEl.addEventListener("click", function setTimer(startGame){
+
+    var seconds = 20;
+
+    var timerInterval = setInterval(function() {
+        seconds--;
+        timerEl.textContent = "Time:" + " " + seconds;
+
+        // if(correct answer is clicekd){
+
+            // then restart the timer and move to the next question 
+            // return timerInterval
+
+        // } else 
+    
+        if(seconds === 0) {
+
+          timerEl.textContent = 'Time: 0';  
+         
+          clearInterval(timerInterval);
+          
+          endGame();
+        }
+    
+      }, 1000);
+
+        // new function ( )
+        // 
+        // 
+        //     
+})
+
+
+
+
+
+
 // ============================================
+
+// startGameEl.addEventListener("click", function setTimer(){
+
+    
+
+// }
+
+
 
 
 // -1 funtion to start the game with start button - this also starts the timer
+// you press the start button and it kick starts the timer.
+// at same time the start button is hidden
+// then the hidden functions are unhidden except for the restart and submit form button.
+// the screen shows the first question
+// the answer choices are shown 
 
 
 
@@ -91,6 +121,9 @@ themeSwitcher.addEventListener("click", function() {
 
 
 // funtion to take you to the next question or previous question
+
+
+//-3 results page and submit button
 
 
 
