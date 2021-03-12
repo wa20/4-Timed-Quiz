98dfe4c8a446f760c8a0a0b3a1854b10762dd065
wael ahmed <w.ahmed020@gmail.com>
2021-03-12 11:24:33 +0000

    added questions to js

--- script.js
+++ script.js
@@ -1,30 +1,146 @@
-
-
-
-
 let question = [
- 
-    { question : "who won the premier league last year",
-      answer: [ "Liverpool", "Man United", "Man City", "Chelsea"],
-      correctAnswer : "Liverpool"
-    },
+  {
+    question:
+      "If soccer is called football in England, what is American football called in England?",
+    answer: ["American football", "Combball", "Handball", "Touchdown"],
+    correctAnswer: "American football",
+  },
 
-    { question : "who came second in the premier league last year",
-      answer: [ "Liverpool", "Man United", "Man City", "Chelsea"],
-      correctAnswer : "Man City"
-    }
+  {
+    question: "What is the largest country in the world?",
+    answer: ["Russia", "Canada", "China", "United States"],
+    correctAnswer: "Russia",
+  },
 
+  {
+    question:
+      "An organic compound is considered an alcohol if it has what functional group?",
+    answer: ["Hydroxyl", "Carbonyl", "Alkyl", "Aldehyde"],
+    correctAnswer: "Hydroxyl",
+  },
+
+  {
+    question: "What is the 100th digit of Pi?",
+    answer: ["9", "4", "7", "2"],
+    correctAnswer: "9",
+  },
+
+  {
+    question: "A doctor with a PhD is a doctor of what?",
+    answer: ["Philosophy", "Pschology", "Phrenology", "Physical Therapy"],
+    correctAnswer: "Philosophy",
+  },
+
+  {
+    question: "What year did World War I begin?",
+    answer: ["1914", "1905", "1919", "1925"],
+    correctAnswer: "1914",
+  },
+
+  {
+    question: "What is isobutylphenylpropanoic acid more commonly known as?",
+    answer: ["Ibuprofen", "Morphine", "Ketamine", "Aspirin"],
+    correctAnswer: "Ibuprofen",
+  },
+
+  {
+    question:
+      "What state is the largest state of the United States of America?",
+    answer: ["Alaska", "California", "Texas", "Washington"],
+    correctAnswer: "Alaska",
+  },
+
+  {
+    question: "What is the tallest mountain in Canada?",
+    answer: [
+      "Mount Logan",
+      "Mont Tremblant",
+      "Whistler Mountain",
+      "Blue Mountain",
+    ],
+    correctAnswer: "Mount Logan",
+  },
+
+  {
+    question: "Which of these is a stop codon in DNA?",
+    answer: ["TAA", "ACT", "ACA", "GTA"],
+    correctAnswer: "TAA",
+  },
+
+  {
+    question: "Which of these countries is NOT a part of the Asian continent?",
+    answer: ["Suriname", "Georgia", "Russia", "Singapore"],
+    correctAnswer: "Suriname",
+  },
+
+  {
+    question: "What is the unit of currency in Laos?",
+    answer: ["Kip", "Ruble", "Konra", "Dollar"],
+    correctAnswer: "Kip",
+  },
+
+  {
+    question: "Which of these is a stop codon in DNA?",
+    answer: ["TAA", "ACT", "ACA", "GTA"],
+    correctAnswer: "TAA",
+  },
+
+  {
+    question: "Which of these animals belongs in class Chondrichthyes?",
+    answer: ["Great white Shark", "Octopus", "Killer whale", "Catfish"],
+    correctAnswer: "Great white shark",
+  },
+
+  {
+    question: "What is the name of the Canadian national anthem?",
+    answer: [
+      "O Canada",
+      "O Red Maple",
+      "Leaf-Spangled Banner",
+      "March of the Puck Drop",
+    ],
+    correctAnswer: "O Canada",
+  },
+
+  {
+    question: "Which of these is NOT an Australian state or territory?",
+    answer: ["Albera", "New South Wales", "Victoria", "Queensland"],
+    correctAnswer: "Alberta",
+  },
+
+  {
+    question: "Where is the Luxor Hotel & Casino located?",
+    answer: [
+      "Paradise, Nevada",
+      "Las Vegas, Nevada",
+      "Winchester, Nevada",
+      "Jackpot, Nevada",
+    ],
+    correctAnswer: "Paradise, Nevada",
+  },
+
+  {
+    question: "In what year did the Wall Street Crash take place?",
+    answer: ["1929", "1932", "1930", "1925"],
+    correctAnswer: "1929",
+  },
+
+  {
+    question: "What is considered the rarist form of color blindness?",
+    answer: ["Blue", "Red", "Green", "Purple"],
+    correctAnswer: "Blue",
+  },
 ];
 
 let currentQsIndex = 0; //shows whatever qs we are displaying
 
+//===========================================
 
-const buttonA = document.getElementById("a");
-const buttonB = document.getElementById("b");
-const buttonC = document.getElementById("c");
-const buttonD = document.getElementById("d");
+// const buttonA = document.getElementById("a"); not needed
+// const buttonB = document.getElementById("b");
+// const buttonC = document.getElementById("c");
+// const buttonD = document.getElementById("d");
 const displayQs = document.getElementById("Qs");
-
 
 //============================================
 // light and Dark Theme Function.
@@ -52,7 +168,7 @@ let timerEl = document.querySelector(".timer");
 let mode = "light";
 
 //theme switching before the game starts
-themeSwitcher.addEventListener("click", function() {
+themeSwitcher.addEventListener("click", function () {
   // If mode is dark, apply light background
   if (mode === "light") {
     mode = "dark";
@@ -88,68 +204,69 @@ startGameEl.addEventListener("click", function () {
       mode = "dark";
       htmlEl.setAttribute("class", "dark");
       headerEl.setAttribute("class", "header-bar-dark");
-    //   screen.setAttribute("class", "screen-black hide"); //remove
-    //   startbtnEl.setAttribute("class", "hide"); // remove
+      //   screen.setAttribute("class", "screen-black hide"); //remove
+      //   startbtnEl.setAttribute("class", "hide"); // remove
 
-      for (i = 0; i < buttonEL.length; i++) {
-        buttonEl[i].setAttribute("class", "ans-btn-dark");
-      }
+      //   for (i = 0; i < buttonEL.length; i++) {
+      //     buttonEl[i].setAttribute("class", "ans-btn-dark");
+      //   }
     } else {
       mode = "light";
       htmlEl.setAttribute("class", "light");
       headerEl.setAttribute("class", "header-bar");
-    //   screen.setAttribute("class", "screen"); //remove
-    //   startbtnEl.setAttribute("class", "hide"); // remove
+      //   screen.setAttribute("class", "screen"); //remove
+      //   startbtnEl.setAttribute("class", "hide"); // remove
 
-      for (i = 0; i < buttonEl.length; i++) {
-        buttonEl[i].setAttribute("class", "ans-btn");
-      }
+      //   for (i = 0; i < buttonEl.length; i++) {
+      //     buttonEl[i].setAttribute("class", "ans-btn");
+      //   }
     }
   });
-  
-  questions();
 
+  questions();
 });
 
+function questions() {
+  // question section, to input question onto display screen
 
-function questions(){
-
-    console.log("questions")
-    displayQs.textContent = question[currentQsIndex].question //this calls the question to the display screen
-    // buttonA.textContent =
-    var buttonArea = document.getElementById("ans-btn"); 
-buttonArea.innerHTML = "";
-    for (var i = 0; i < question[currentQsIndex].answer.length; i++){
-        var button = document.createElement("button");
-        button.setAttribute("class", "ans-btn");
-        button.textContent = question[currentQsIndex].answer[i];
-        button.onclick = checkAnswer;
-        buttonArea.appendChild(button); 
-    }
-
-
-
-
-
-}
-function checkAnswer(){
-    console.log(this.textContent); 
-    if(this.textContent === question[currentQsIndex].correctAnswer){
-        alert("Correct")
-    } else {
-        alert("Incorrect")
-    }
-    currentQsIndex++;
-    if(currentQsIndex === question.length){
-        endGame()
-    }else {
-        questions();
-    }
+  console.log("questions");
+  displayQs.textContent = question[currentQsIndex].question; //this calls the question to the display screen
+  // buttonA.textContent =
+  var buttonArea = document.getElementById("ans-btn"); // varaible created from ans-btn section
+  buttonArea.innerHTML = "";
+  for (var i = 0; i < question[currentQsIndex].answer.length; i++) {
+    var button = document.createElement("button");
+    button.setAttribute("class", "ans-btn");
+    button.textContent = question[currentQsIndex].answer[i];
+    button.onclick = checkAnswer;
+    buttonArea.appendChild(button);
+  }
 }
 
+function checkAnswer() {
+  // check if answer is correct
+  console.log(this.textContent);
+  if (this.textContent === question[currentQsIndex].correctAnswer) {
+    button.setAttribute("class", "correct");
 
+    alert("Correct");
+    // setTimer()
+  } else {
+    alert("Incorrect");
+    second -= 3;
+    timerEl.textContent = "Time:" + " " + seconds;
+  }
+  currentQsIndex++;
+  if (currentQsIndex === question.length) {
+    endGame();
+  } else {
+    questions();
+  }
+}
+
+var seconds;
 startGameEl.addEventListener("click", function setTimer() {
-  var seconds = 20;
+  seconds = 20;
 
   var timerInterval = setInterval(function () {
     seconds--;
@@ -179,25 +296,12 @@ startGameEl.addEventListener("click", function setTimer() {
 
 // }
 
-
-
-
-function endGame() {
-
-
-}
-
-
-
-
+function endGame() {}
 
 // submitButtonEl.addEventListener("click", function submit(){
 
-
 // })
-
 
 // playAgainEl.addEventListener("click", function playAgain(){
 
-    
 // })
